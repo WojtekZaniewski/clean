62a73406b49f478ff8f1cadc9998966eb254cd0c
WojtekZaniewski <wojtekzaniewski@icloud.com>
2026-01-30 14:18:56 +0100

    aa

--- components/why-us-section.tsx
+++ components/why-us-section.tsx
@@ -64,7 +64,7 @@ export default function WhyUsSection() {
                 backgroundPosition: 'center center'
               }}
               role="img"
-              aria-label="Lena's Cleaning - Nettoyage professionnel à Liège"
+              aria-label="Lena's Cleaning - Nettoyage à Liège"
             >
             </div>
           </div>
