30f9d0044627fd3bc9c46bbc9e27b6ca5c5c9c1a
WojtekZaniewski <wojtekzaniewski@icloud.com>
2026-01-30 14:31:17 +0100

    aa

--- scripts/copy-to-root.js
+++ scripts/copy-to-root.js
@@ -11,6 +11,7 @@ const filesToCopy = [
   '404.html',
   '.nojekyll',
   'robots.txt',
+  'sitemap.xml',
   '_next',
   '1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg',
   '10.jpg', '11.jpg', '12.jpg', '13.jpg',
